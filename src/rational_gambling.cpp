c6f2039119f96bfeee49db45987c18c8eb668b11
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2025-04-19 11:47:10 -0700

    bet a fixed fraction and stop

--- src/rational_gambling.cpp
+++ src/rational_gambling.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <random>
 
-namespace parameters {
-    constexpr int ROUNDS = 10'000;
-    constexpr double WIN_PROBABILITY = 0.55;
-    constexpr double PAYOUT_RATIO = 1.0;
-    constexpr double STARTING_BANKROLL = 1.0;
-    constexpr double BET_PERCENT = 0.1;
-}
+constexpr int ROUNDS = 10'000;
+constexpr double WIN_PROBABILITY = 0.55;
+constexpr double PAYOUT_RATIO = 1.0;
+constexpr double STARTING_BANKROLL = 1.0;
+constexpr double BET_PERCENT = 0.1;
 
 bool flip_coin(double probability_of_winning) {
     std::random_device random;
@@ -17,15 +15,29 @@ bool flip_coin(double probability_of_winning) {
     return result;
 }
 
+bool bankroll_condition_met(double bankroll) {
+    bool return_value;
+    if (bankroll >= 2 * STARTING_BANKROLL) {
+        return_value = true;
+    } else {
+        return_value = false;
+    }
+    return return_value;
+}
+
 int main() {
-    double bankroll = parameters::STARTING_BANKROLL;
-    for (int j = 0; j < parameters::ROUNDS; j++) {
-        double bet_size = bankroll * parameters::BET_PERCENT;
-        bool result = flip_coin(parameters::WIN_PROBABILITY);
+    double bankroll = STARTING_BANKROLL;
+    for (int j = 0; j < ROUNDS; j++) {
+        double bet_size = bankroll * BET_PERCENT;
+        bool result = flip_coin(WIN_PROBABILITY);
         if (result) {
-            bankroll += (parameters::PAYOUT_RATIO * bet_size);
+            bankroll += (PAYOUT_RATIO * bet_size);
         } else {
             bankroll -= bet_size;
+        }
+        if (bankroll_condition_met(bankroll)) {
+            std::cout << "Starting amount doubled after " << (j + 1) << " rounds" << std::endl;
+            break;
         }
     }
     std::cout << "Ending bankroll: " << bankroll << std::endl;
