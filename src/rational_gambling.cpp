8784948e66fa6df20c17bed967ed77b3f25712cd
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2025-04-19 12:31:27 -0700

    test from a set of bet percents

--- src/rational_gambling.cpp
+++ src/rational_gambling.cpp
@@ -26,8 +26,8 @@ bool bankroll_condition_met(double bankroll) {
     return return_value;
 }
 
-double simulate_single_round(double current_bankroll) {
-    double bet_size = current_bankroll * BET_PERCENT;
+double simulate_single_round(double current_bankroll, double bet_percent) {
+    double bet_size = current_bankroll * bet_percent;
     bool result = flip_coin(WIN_PROBABILITY);
     if (result) {
         current_bankroll += (PAYOUT_RATIO * bet_size);
@@ -37,12 +37,12 @@ double simulate_single_round(double current_bankroll) {
     return current_bankroll;
 }
 
-bool simulate_rounds() {
+bool simulate_rounds(double bet_percent) {
     bool bankroll_doubled = false;
     double bankroll = STARTING_BANKROLL;
     int current_round = 1;
     while (!bankroll_doubled && current_round <= ROUNDS) {
-        bankroll = simulate_single_round(bankroll);
+        bankroll = simulate_single_round(bankroll, bet_percent);
         if (bankroll_condition_met(bankroll)) {
             bankroll_doubled = true;
         }
@@ -51,14 +51,30 @@ bool simulate_rounds() {
     return bankroll_doubled;
 }
 
-int main() {
+double get_success_probability(double bet_percent) {
     int successes = 0;
     for (int i = 0; i < ITERATIONS; i++) {
-        bool success = simulate_rounds();
+        bool success = simulate_rounds(bet_percent);
         if (success) {
             successes++;
         }
     }
     double success_probability = static_cast<double>(successes) / static_cast<double>(ITERATIONS);
-    std::cout << "Probability of success: " << success_probability << std::endl;
+    return success_probability;
+}
+
+int main() {
+    std::vector<double> bet_percents = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    double best_bet_percent;
+    double best_success_probability;
+    for (double bet_percent: bet_percents) {
+        std::cout << "Testing " << bet_percent << " bet percent" << std::endl;
+        double success_probability = get_success_probability(bet_percent);
+        if (success_probability >= best_success_probability) {
+            best_bet_percent = bet_percent;
+            best_success_probability = success_probability;
+        }
+    }
+    std::cout << "Best bet percent is " << best_bet_percent << " with a " << best_success_probability
+              << " success probability" << std::endl;
 }
